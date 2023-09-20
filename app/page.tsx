fab095a77851cc9c5523b6b4a1806300b1774ede
sangwopark19 <sangwopark19@gmail.com>
2023-09-20 23:12:36 +0900

    그전에 추가한 Button컴포넌트 page에 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
 export default function Home() {
   return (
-    <p className='text-3xl font-bold text-indigo-500'>Hello Discord Clone</p>
+    <div className="flex flex-col">
+      <p className='text-3xl font-bold text-indigo-500'>
+        Hello Discord Clone'
+      </p>
+    <Button>Click me</Button>
+    </div>
   )
 }
