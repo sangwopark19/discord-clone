948a8d5473d885772a8226fd8425f547fecd8a6f
sangwopark19 <sangwopark19@gmail.com>
2023-09-21 22:32:10 +0900

    button컴포넌트 속성 추가 및 페이지의 버튼객체에 추가한 속성 추가

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
       <p className='text-3xl font-bold text-indigo-500'>
         Hello Discord Clone'
       </p>
-    <Button>Click me</Button>
+      <Button variant="test">
+        Click me
+      </Button>
     </div>
   )
 }
