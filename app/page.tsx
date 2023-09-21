63b88fb53098626dc7a8af8aeab92b9d8d492087
sangwopark19 <sangwopark19@gmail.com>
2023-09-21 22:27:23 +0900

    전체 넓이의 div 속성 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div>
       <p className='text-3xl font-bold text-indigo-500'>
         Hello Discord Clone'
       </p>
