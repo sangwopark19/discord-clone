6daf9cf62ace9ca043f4cc6e63e1044705f72278
sangwopark19 <sangwopark19@gmail.com>
2023-09-21 22:42:40 +0900

    layout.tsx font, title 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 
 export default function Home() {
   return (
     <div>
       <p className='text-3xl font-bold text-indigo-500'>
-        Hello Discord Clone'
+        Hello Discord Clone
       </p>
-      <Button variant="test">
+      <Button>
         Click me
       </Button>
     </div>
